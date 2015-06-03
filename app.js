02f526fb79271e0a8d2c783af7532324e909df1e
Thomas Boyt <thomas@ledgerx.com>
2015-06-03 03:11:14 -0400

    More complex variable-length fragments (...are currently broken)

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 import React from 'react-tools/build/npm-react';
 import _ from 'lodash';
 
-class Item extends React.Component {
-  render() {
-    return (
-      <li>{this.props.children}</li>
-    );
-  }
-}
-
 class ListItems extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: [1, 2],
+      next: 3
+    };
+  }
+
+  handleAdd() {
+    this.setState({
+      list: this.state.list.concat(this.state.next),
+      next: this.state.next + 1
+    });
+  }
+
+  handleRemove(num) {
+    this.setState({
+      list: _.without(this.state.list, num)
+    });
+  }
+
+  renderItems() {
+    return this.state.list.map((item, idx) => {
+      return (
+        <li key={idx}>{this.props.num} {'->'} {item} <button onClick={() => this.handleRemove(item)}>-</button></li>
+      );
+    });
+  }
+
   render() {
     return (
       <frag>
-        <Item>foo {this.props.num}</Item>
-        <Item>bar {this.props.num}</Item>
-        <button onClick={this.props.onRemove}>remove</button>
+        {this.renderItems()}
+        <li>
+          <button onClick={() => this.handleAdd()}>+ add</button>
+          <button onClick={this.props.onRemove}>remove all</button>
+        </li>
       </frag>
     );
   }
@@ -26,8 +50,9 @@ class Main extends React.Component {
     super(props);
 
     this.state = {
-      list: [1, 2, 3, 4, 5],
-      next: 6
+      list: [1, 2],
+      next: 3,
+      showThing: false
     };
   }
 
@@ -61,17 +86,18 @@ class Main extends React.Component {
 
   render() {
     return (
-      <frag>
+      <div>
         <ul>
           {this.renderItems()}
         </ul>
+        {this.state.showThing && <p>hi</p>}
         <button onClick={() => this.handleShuffle()}>
           Shuffle
         </button>
         <button onClick={() => this.handleAdd()}>
           Append to End
         </button>
-      </frag>
+      </div>
     );
   }
 }
